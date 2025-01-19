e8275d9a432eb5e24b604bb5fec8b0c9e7428a08
Prayuj Tuli <prayujtuli@hotmail.com>
2025-01-19 12:33:54 -0500

    refactor: adjusted production values and created separate population store

--- internal/constants/buildings.go
+++ internal/constants/buildings.go
@@ -13,22 +13,31 @@ var buildingProduction = map[string][]int64{
 	BUILDING_TYPE_CITY_CENTER: {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000},
 	BUILDING_TYPE_TOWN_CENTER: {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000},
 	BUILDING_TYPE_BARRACKS:    {50, 100, 150, 200, 250, 300, 350, 400, 450, 500},
-	BUILDING_TYPE_HOUSE:       {20, 40, 60, 80, 100, 120, 140, 160, 180, 200},
 	BUILDING_TYPE_FARM:        {10, 20, 30, 40, 50, 60, 70, 80, 90, 100},
 	BUILDING_TYPE_MINE:        {30, 60, 90, 120, 150, 180, 210, 240, 270, 300},
 }
 
-var buildingCosts = map[string][]int64{
+var buildingPopulation = map[string][]int64{
 	BUILDING_TYPE_CITY_CENTER: {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000},
 	BUILDING_TYPE_TOWN_CENTER: {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000},
-	BUILDING_TYPE_BARRACKS:    {50, 100, 150, 200, 250, 300, 350, 400, 450, 500},
-	BUILDING_TYPE_HOUSE:       {20, 40, 60, 80, 100, 120, 140, 160, 180, 200},
-	BUILDING_TYPE_FARM:        {10, 20, 30, 40, 50, 60, 70, 80, 90, 100},
-	BUILDING_TYPE_MINE:        {30, 60, 90, 120, 150, 180, 210, 240, 270, 300},
+	BUILDING_TYPE_HOUSE:       {50, 100, 150, 200, 250, 300, 350, 400, 450, 500},
+}
+
+var buildingCosts = map[string][]int64{
+	BUILDING_TYPE_CITY_CENTER: {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000},
+	BUILDING_TYPE_TOWN_CENTER: {1000, 200, 300, 400, 500, 600, 700, 800, 900, 10000},
+	BUILDING_TYPE_BARRACKS:    {500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000},
+	BUILDING_TYPE_HOUSE:       {200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000},
+	BUILDING_TYPE_FARM:        {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000},
+	BUILDING_TYPE_MINE:        {300, 600, 900, 1200, 1500, 1800, 2100, 2400, 2700, 3000},
 }
 
 func GetBuildingProduction(buildingType string, level int) int64 {
 	return buildingProduction[buildingType][level]
+}
+
+func GetBuildingPopulation(buildingType string, level int) int64 {
+	return buildingPopulation[buildingType][level]
 }
 
 func GetBuildingCost(buildingType string, level int) int64 {
